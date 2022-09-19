dcce8d7a8fe84a091b1005f95b087b046acb7711
lozo6 <lorenzo.francisco@stonybrook.edu>
2022-09-19 09:05:38 -0400

    first exercise 1

--- scipy.py
+++ scipy.py
@@ -29,3 +29,15 @@ groupby_gender.mean() # checks mean or average of all columns
 
 ### EXERCISE ###
 
+# What is the mean value for VIQ for the full population?
+print(data[['VIQ']].mean())
+
+# How many males/females were included in this study?
+groupby_gender = data.groupby('Gender')
+print(groupby_gender.count())
+
+# What is the average value of MRI counts expressed in log units, for males and females?
+data['MRI_Log_Count'] = np.log10(data['MRI_Count']) # first create a new column with log10 values using numpy
+groupby_gender = data.groupby('Gender')
+for gender, value in groupby_gender['MRI_Log_Count']:
+    print((gender, value.mean()))
